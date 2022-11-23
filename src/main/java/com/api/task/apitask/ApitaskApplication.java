6e64e496def1b2323ab1da9786b30b9343254dad
hosamk92 <hosamk92@gmail.com>
2022-11-23 20:32:44 +0200

    feat: [BE-1] adding first task

---
+++ src/main/java/com/api/task/apitask/ApitaskApplication.java
@@ -0,0 +1,13 @@
+package com.api.task.apitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/task/apitask/ApitaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.task.apitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
